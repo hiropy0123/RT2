c4aa139222ce84cfc0d1b1c9a57572eba79ac1ff
HirokiNakashima <hiroki123naka4ma@gmail.com>
2018-03-04 19:03:40 +0900

    user register integration test

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,11 @@ Rails.application.routes.draw do
 
   resources :users
 
-  get 'static_pages/about'
-  get 'static_pages/contact'
+  get '/about', to: 'static_pages#about'
+  get '/contact', to: 'static_pages#contact'
   get 'static_pages/help'
+  get '/signup', to: 'users#new'
+  post 'signup', to: 'users#create'
 
   root 'static_pages#home'
 
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -13,13 +13,13 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should get about" do
-    get static_pages_about_url
+    get about_url
     assert_response :success
     assert_select "title", "About | #{@base_title}"
   end
 
   test "should get contact" do
-    get static_pages_contact_url
+    get contact_url
     assert_response :success
     assert_select "title", "Contact | #{@base_title}"
   end
