8c992ee45d6aec87c4d097499c147732863dac4a
HirokiNakashima <hiroki123naka4ma@gmail.com>
2018-03-04 18:23:10 +0900

    ユーザー登録の失敗

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,5 +4,28 @@ class UsersController < ApplicationController
   end
 
   def new
+    @user = User.new
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      # 保存の成功
+
+    else
+      # 保存に失敗した時はnewページに戻る
+      render 'new'
+    end
+  end
+
+
+  private
+    def user_params
+      params.require(:user).permit(
+        :name,
+        :email,
+        :password,
+        :password_confirmation
+      )
+    end
 end
