cb5e6c46afa899a9031ccfeaa722f6a11df8fbe0
HirokiNakashima <hiroki123naka4ma@gmail.com>
2018-03-06 16:06:48 +0900

    最終更新が何日前かを表示

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -6,4 +6,12 @@ module UsersHelper
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
     image_tag(gravatar_url, alt: user.name, class: "gravatar")
   end
+
+  def last_update_time
+    # 最終更新が何日前かを表示
+    time_now = Time.now
+    sec = time_now - @user.updated_at
+    last_update_time = (Time.parse("1/1") + sec - (day = sec.to_i / 86400) * 86400).strftime("#{day}日%H時間%M分%S秒")
+    "#{last_update_time}前"
+  end
 end
