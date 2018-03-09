a77067244bc65540ab9e6a8a576aeb6d448dac53
HirokiNakashima <hiroki123naka4ma@gmail.com>
2018-03-09 23:08:35 +0900

    Session Controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
 Rails.application.routes.draw do
 
-  resources :users
-
   get '/about', to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
   get 'static_pages/help'
   get '/signup', to: 'users#new'
   post 'signup', to: 'users#create'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+
+  resources :users
 
   root 'static_pages#home'
 
