d8663f1d92a84c243d2a8a4fe0aa094843af839c
HirokiNakashima <hiroki123naka4ma@gmail.com>
2018-03-10 14:02:45 +0900

    flash test

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,19 @@
 class SessionsController < ApplicationController
   def new
+
+  end
+
+  def create
+    user = User.find_by( email: params[:session][:email].downcase )
+    if user && user.authenticate(params[:session][:password])
+      # ログインフォームで入力されたメールアドレスからユーザーの存在を確認し、さらにパスワードが有効かどうか
+    else
+      flash.now[:danger] = '入力されたメールアドレスとパスワードの組み合わせが正しくありません。'
+      render 'new'
+    end
+  end
+
+  def destroy
+
   end
 end
