55a56d2ea2b1a564e1f5e5bbf766dc47f03a1b7a
HirokiNakashima <hiroki123naka4ma@gmail.com>
2018-03-10 14:39:40 +0900

    session helper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  # アプリ全体からアクセス可能にする
+  include SessionsHelper
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,6 +7,8 @@ class SessionsController < ApplicationController
     user = User.find_by( email: params[:session][:email].downcase )
     if user && user.authenticate(params[:session][:password])
       # ログインフォームで入力されたメールアドレスからユーザーの存在を確認し、さらにパスワードが有効かどうか
+      log_in user
+      redirect_to user
     else
       flash.now[:danger] = '入力されたメールアドレスとパスワードの組み合わせが正しくありません。'
       render 'new'
