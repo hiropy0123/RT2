38d64d515369346c1c1e6a773b6c3971aa8ab76a
HirokiNakashima <hiroki123naka4ma@gmail.com>
2018-03-11 22:23:17 +0900

    remember メソッドをUserモデルに追加する

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  # attr_accessorでアクセス可能なremember_token属性を作成
+  attr_accessor :remember_token
+
   before_save { self.email = email.downcase }
   validates :name,
             presence: true,
@@ -24,6 +27,14 @@ class User < ApplicationRecord
   # ランダムなトークンを生成し返す
   def User.new_token
     SecureRandom.urlsafe_base64
+  end
+
+  # 永続セッションのためにユーザーをデータベースに記憶する
+  def remember
+    # 自身のremember_token属性に生成されたトークンを保存
+    self.remember_token = User.new_token
+    # データベースに保存するのはremeber_digest
+    update_attribute(:remeber_digest, User.digest(remember_token))
   end
 
 end
